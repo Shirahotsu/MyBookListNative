9e61007f3b1b40f80c595abb295e9388f4fad180
aerdukant <aerdukant@bee2code.com>
2022-11-12 10:28:57 +0100

    Added voice to text in book adding

--- android/app/src/main/java/com/mybooklist/MainApplication.java
+++ android/app/src/main/java/com/mybooklist/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.mybooklist.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.wenkesj.voice.VoicePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
--- screens/AddBook.screen.tsx
+++ screens/AddBook.screen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Text, TextInput, View } from "../components/Themed";
 import useColorScheme from "../hooks/useColorScheme";
 import Colors from "../constants/Colors";
@@ -7,18 +7,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faCheckSquare, faSquare } from "@fortawesome/free-regular-svg-icons";
 import FontSize from "../constants/FontSize";
 import { TouchableOpacity } from "react-native";
-import { Book } from "../models/Book.model";
 import { addBook } from "../firebase/bookList.firebase";
 import { useToast } from "react-native-toast-notifications";
 import { RootTabScreenProps } from "../types";
+import Voice, { SpeechErrorEvent, SpeechResultsEvent } from "@react-native-community/voice";
+import { faClose, faMicrophone } from "@fortawesome/free-solid-svg-icons";
 
-const AddBookScreen = ({navigation}: RootTabScreenProps<'Search'>) => {
+const AddBookScreen = ({ navigation }: RootTabScreenProps<"Search">) => {
+
+  const onSpeechError = (e: SpeechErrorEvent) => {
+    setInputVoice('')
+    Voice.destroy().then(Voice.removeAllListeners)
+  };
+
+  const onSpeechResults = async (e: SpeechResultsEvent) => {
+    if (e?.value) {
+      setResultsToInput(e?.value[0]);
+      Voice.destroy().then(Voice.removeAllListeners)
+    }
+  };
+
+
+  Voice.onSpeechError = onSpeechError;
+  Voice.onSpeechResults = onSpeechResults;
+  let inputToBeSet = ''
+
   const toast = useToast();
   const colorScheme = useColorScheme();
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [releaseDate, setReleaseDate] = useState('2022-01-01')
-  const [pages, setPages] = useState('0')
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [releaseDate, setReleaseDate] = useState("2022-01-01");
+  const [pages, setPages] = useState("");
+  const [inputVoice, setInputVoice] = useState("");
   const [categories, setCategories] = useState([
     {
       label: "Fantastyka",
@@ -50,52 +70,129 @@ const AddBookScreen = ({navigation}: RootTabScreenProps<'Search'>) => {
     },
   ]);
 
-  const selectCategory = ({label, selected}) =>{
-    const newCategories = [...categories].map(category=>{
-      if(category.label === label){
-        category.selected = !selected
-      }
-      return category
-    })
-    setCategories(newCategories)
-  }
+  const startRecognizing = async (input: string) => {
+    inputToBeSet = input
+    setInputVoice(input);
+    try {
+      await Voice.start("pl-pl");
+    } catch (e) {
+      toast.show("Ups! Coś poszło nie tak", { type: "danger" });
+      await cancelRecognizing();
+    }
+  };
 
-  const handleOnSubmit = async () =>{
-    if(pages.length < 1
+  const cancelRecognizing = async () => {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const setResultsToInput = (results: string) => {
+    if (inputToBeSet === "title") {
+      setTitle(results);
+    } else if (inputToBeSet === "description") {
+      setDescription(results);
+    } else if (inputToBeSet === "pages") {
+      setPages(results);
+    }
+    setInputVoice("");
+  };
+
+  const selectCategory = ({ label, selected }) => {
+    const newCategories = [...categories].map(category => {
+      if (category.label === label) {
+        category.selected = !selected;
+      }
+      return category;
+    });
+    setCategories(newCategories);
+  };
+
+  const handleOnSubmit = async () => {
+    if (pages.length < 1
       && title.length < 3
       && description.length < 3
       && categories.length < 1
       && !isNaN(parseInt(pages))
-    ){
-      toast.show('Błędne dane', {type: 'danger'});
-      return
+    ) {
+      toast.show("Błędne dane", { type: "danger" });
+      return;
     }
     const newBook = {
       title,
       description,
       releaseDate,
       pages: parseInt(pages),
-      categories: categories.filter(v=>v.selected).map(v=>v.label)
-    }
-    const result = await addBook(newBook)
-    if(result){
-      toast.show('Dodano', {type: 'success'});
-      navigation.navigate('Search')
+      categories: categories.filter(v => v.selected).map(v => v.label),
+    };
+    const result = await addBook(newBook);
+    if (result) {
+      toast.show("Dodano", { type: "success" });
+      navigation.navigate("Search");
     } else {
-      toast.show('Ups! Coś poszło nie tak', {type: 'danger'});
+      toast.show("Ups! Coś poszło nie tak", { type: "danger" });
     }
-  }
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: Colors[colorScheme].background, paddingHorizontal: Spacing.md }}>
-      <TextInput onChangeText={setTitle} style={{ marginTop: Spacing.md }} placeholder={"Tytuł"} />
-      <TextInput onChangeText={setDescription} multiline={true} style={{ minHeight: 30, marginTop: Spacing.md }} placeholder={"Opis"} />
-      <TextInput onChangeText={setPages} style={{ minHeight: 30, marginTop: Spacing.md }} placeholder={"Strony"} />
-      <TextInput  value={releaseDate} style={{ marginTop: Spacing.md }} placeholder={"Data wydania"} editable={false} />
+      <Text>{inputVoice}</Text>
+      <View style={{ flexDirection: "row", alignItems: "center", marginTop: Spacing.md }}>
+        <TextInput value={title} onChangeText={setTitle} style={{ flex: 1 }} placeholder={"Tytuł"} />
+        <TouchableOpacity onPress={() => startRecognizing("title")}>
+          <View style={{
+            marginLeft: Spacing.md,
+            backgroundColor: Colors.dark.tint,
+            height: 40,
+            width: 40,
+            borderRadius: 100,
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}>
+            <FontAwesomeIcon color={"white"} icon={inputVoice === "title" ? faClose : faMicrophone} />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row", alignItems: "center", marginTop: Spacing.md }}>
+        <TextInput value={description} onChangeText={setDescription} multiline={true} style={{ minHeight: 30, flex: 1 }}
+                   placeholder={"Opis"} />
+        <TouchableOpacity onPress={() => startRecognizing("description")}>
+          <View style={{
+            marginLeft: Spacing.md,
+            backgroundColor: Colors.dark.tint,
+            height: 40,
+            width: 40,
+            borderRadius: 100,
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}>
+            <FontAwesomeIcon color={"white"} icon={inputVoice === "description" ? faClose : faMicrophone} />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row", alignItems: "center", marginTop: Spacing.md }}>
+        <TextInput value={pages} onChangeText={setPages} style={{ minHeight: 30, flex: 1 }} placeholder={"Strony"} />
+        <TouchableOpacity onPress={() => startRecognizing("pages")}>
+          <View style={{
+            marginLeft: Spacing.md,
+            backgroundColor: Colors.dark.tint,
+            height: 40,
+            width: 40,
+            borderRadius: 100,
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}>
+            <FontAwesomeIcon color={"white"} icon={inputVoice === "pages" ? faClose : faMicrophone} />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <TextInput value={releaseDate} style={{ marginTop: Spacing.md }} placeholder={"Data wydania"} editable={false} />
       <View style={{ marginTop: Spacing.md }}>
         {
           categories.map((category, index) =>
-            <TouchableOpacity onPress={()=>selectCategory(category)}>
+            <TouchableOpacity onPress={() => selectCategory(category)}>
               <View key={index} style={{ flexDirection: "row", alignItems: "center", marginTop: Spacing.md }}>
                 <FontAwesomeIcon
                   icon={category.selected ? faCheckSquare : faSquare}
@@ -108,11 +205,21 @@ const AddBookScreen = ({navigation}: RootTabScreenProps<'Search'>) => {
           )
         }
       </View>
-    <TouchableOpacity>
-      <Text style={{marginTop:Spacing.lg, marginBottom:Spacing.md, textAlign:'center', fontSize: FontSize.h3, textDecorationStyle: 'solid', textDecorationLine: 'underline', textDecorationColor: Colors[colorScheme].text}}>Dodaj okładkę...</Text>
-    </TouchableOpacity>
-      <View style={{marginTop: Spacing.xl}}>
-        <Button title={'Dodaj książkę'} onPress={()=>{handleOnSubmit()}}/>
+      <TouchableOpacity>
+        <Text style={{
+          marginTop: Spacing.lg,
+          marginBottom: Spacing.md,
+          textAlign: "center",
+          fontSize: FontSize.h3,
+          textDecorationStyle: "solid",
+          textDecorationLine: "underline",
+          textDecorationColor: Colors[colorScheme].text,
+        }}>Dodaj okładkę...</Text>
+      </TouchableOpacity>
+      <View style={{ marginTop: Spacing.xl }}>
+        <Button title={"Dodaj książkę"} onPress={() => {
+          handleOnSubmit();
+        }} />
       </View>
     </View>
   );
