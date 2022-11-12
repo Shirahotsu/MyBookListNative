f7c9d6049ced09955cee9c99e2371a0385dfabfb
aerdukant <aerdukant@bee2code.com>
2022-11-12 12:39:05 +0100

    Login changes

--- screens/LogIn.tsx
+++ screens/LogIn.tsx
@@ -1,71 +1,126 @@
-import { ImageBackground, ScrollView, StyleSheet, TouchableHighlight, TouchableOpacity, View } from "react-native";
-import {useToast} from 'react-native-toast-notifications';
+import { ImageBackground, StyleSheet, TouchableOpacity, View } from "react-native";
+import { useToast } from "react-native-toast-notifications";
 
-import {Button, TextInput, Text} from '../components/Themed';
-import Spacing from '../constants/Spacing';
-import {login} from '../firebase/main.firebase';
-import React, {useState} from 'react';
+import { Button, Text, TextInput } from "../components/Themed";
+import Spacing from "../constants/Spacing";
+import { login, register } from "../firebase/main.firebase";
+import React, { useState } from "react";
 import FontSize from "../constants/FontSize";
+import { createProfileDetails, loadProfileDetails } from "../firebase/profile.firebase";
 
-export default function LogIn({navigation}: any) {
-  const image = require('../assets/images/auth-screen.png')
-  console.log('HALOOOO');
+export default function LogIn({ navigation }: any) {
+  const image = require("../assets/images/main-auth-bg.png");
+  console.log("HALOOOO");
   const toast = useToast();
-
+  const [isInLoginView, setIsInLoginView] = useState(true);
   const [loginParams, setLoginParams] = useState({
-    email: 'arek.erdu@o2.pl',
-    password: 'wsadwsad1',
+    email: "arek.erdu@o2.pl",
+    password: "wsadwsad1",
   });
 
   const setEmail = value => {
-    setLoginParams({...loginParams, email: value});
+    setLoginParams({ ...loginParams, email: value });
   };
   const setPassword = value => {
-    setLoginParams({...loginParams, password: value});
+    setLoginParams({ ...loginParams, password: value });
   };
 
   const onLogIn = async () => {
     const response = await login(loginParams.email, loginParams.password);
     if (response.success) {
-      toast.show('Pomyślnie zalogowano', {type: 'success'});
+      toast.show("Pomyślnie zalogowano", { type: "success" });
     } else {
-      toast.show(String(response.value), {type: 'danger'});
+      toast.show(String(response.value), { type: "danger" });
     }
+  };
+
+  const onSignIn = async () => {
+    console.log("%c onSignIn", "color:fuchsia");
+    const response = await register(loginParams.email, loginParams.password);
+    if (response.success) {
+      console.log("response", response);
+      await createProfileDetails(response.value.user.uid);
+      await loadProfileDetails();
+      toast.show("Pomyślnie zarejestrowano się", { type: "success" });
+    } else {
+      toast.show(String(response.value), { type: "danger" });
+    }
+  };
+
+  const changeView = () => {
+    setLoginParams({email: '', password: ''})
+    setIsInLoginView(!isInLoginView);
   };
 
   return (
     <View style={s.container}>
       <ImageBackground source={image} resizeMode="cover" style={s.image}>
-      <View style={s.basicInfo}>
-        <TextInput
-          onChangeText={setEmail}
-          value={loginParams.email}
-          placeholder={'Email'}
-          style={{marginTop:Spacing.sm}}
-        />
-        <TextInput
-          onChangeText={setPassword}
-          value={loginParams.password}
-          placeholder={'Password'}
-          secureTextEntry={true}
-          style={{marginTop:Spacing.sm}}
-        />
-        <View style={{marginTop:Spacing.md, marginBottom: Spacing.lg}}>
-          <Button title={'Zaloguj się'} onPress={onLogIn} />
-        </View>
-        <View style={{marginTop:Spacing.xl, justifyContent:"space-around", alignItems: "center"}}>
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize: FontSize.h5}}>Nie masz konta?</Text>
-            <TouchableOpacity
-              onPress={() => {
-              navigation.push('SignIn');
-            }}
-            >
-              <Text style={{fontSize: FontSize.h5, marginLeft:Spacing.sm, borderBottomColor:'white', borderBottomWidth: 1}}>Zarejestruj się</Text>
-            </TouchableOpacity>
+        <View style={{ marginTop: -150 }}>
+          <View style={s.logo}>
+            <ImageBackground source={require("../assets/images/main-logo.png")} resizeMode="cover" style={s.image} />
+          </View>
+          <View style={s.basicInfo}>
+            <TextInput
+              onChangeText={setEmail}
+              value={loginParams.email}
+              placeholder={"Email"}
+              style={{ marginTop: Spacing.sm }}
+            />
+            <TextInput
+              onChangeText={setPassword}
+              value={loginParams.password}
+              placeholder={"Password"}
+              secureTextEntry={true}
+              style={{ marginTop: Spacing.sm }}
+            />
+            <View style={{ marginTop: Spacing.md, marginBottom: Spacing.lg }}>
+              {
+                isInLoginView
+                  ? <Button title={"Zaloguj się"} onPress={onLogIn} />
+                  : <Button title={"Zarejestruj się"} onPress={onSignIn} />
+              }
+
+            </View>
+            <View style={{ marginTop: Spacing.xl, justifyContent: "space-around", alignItems: "center" }}>
+              <View style={{ flexDirection: "row" }}>
+                {
+                  isInLoginView ?
+                    <>
+                      <Text style={{ fontSize: FontSize.h5 }}>Nie masz konta?</Text>
+                      <TouchableOpacity
+                        onPress={() => {
+                          changeView();
+                        }}
+                      >
+                        <Text style={{
+                          fontSize: FontSize.h5,
+                          marginLeft: Spacing.sm,
+                          borderBottomColor: "white",
+                          borderBottomWidth: 1,
+                        }}>Zarejestruj się</Text>
+                      </TouchableOpacity>
+                    </>
+                    :
+                    <>
+                      <TouchableOpacity
+                        onPress={() => {
+                          changeView();
+                        }}
+                      >
+                        <Text style={{
+                          fontSize: FontSize.h5,
+                          marginLeft: Spacing.sm,
+                          borderBottomColor: "white",
+                          borderBottomWidth: 1,
+                        }}>Wróć do logowania</Text>
+                      </TouchableOpacity>
+                    </>
+                }
+
+              </View>
+            </View>
           </View>
         </View>
-      </View>
       </ImageBackground>
     </View>
   );
@@ -74,21 +129,25 @@ export default function LogIn({navigation}: any) {
 const s = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    flexDirection: 'column',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+
   basicInfo: {
-    marginVertical: Spacing.xl,
-    width: '100%'
+    marginTop: 80,
+    marginBottom: Spacing.xs,
+    width: "100%",
+    backgroundColor: "rgba(52, 52, 52, 0.5)",
+    paddingHorizontal: Spacing.md,
+    paddingVertical: Spacing.xl,
   },
   image: {
     flex: 1,
     justifyContent: "center",
-    with: '100%'
+    with: "100%",
+  },
+  logo: {
+    height: 128,
+    width: "100%",
+    justifyContent: "flex-start",
+    paddingHorizontal: Spacing.xl,
   },
 });
--- screens/SingIn.tsx
+++ screens/SingIn.tsx
@@ -1,90 +1,81 @@
-import { ImageBackground, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import { ImageBackground, StyleSheet, TouchableOpacity, View } from "react-native";
 
-import {Button, Text, TextInput} from '../components/Themed';
-import Spacing from '../constants/Spacing';
-import {register} from '../firebase/main.firebase';
-import {
-  createProfileDetails,
-  loadProfileDetails,
-} from '../firebase/profile.firebase';
-import React, {useState} from 'react';
-import {useToast} from 'react-native-toast-notifications';
+import { Button, Text, TextInput } from "../components/Themed";
+import Spacing from "../constants/Spacing";
+import { register } from "../firebase/main.firebase";
+import { createProfileDetails, loadProfileDetails } from "../firebase/profile.firebase";
+import React, { useState } from "react";
+import { useToast } from "react-native-toast-notifications";
 import FontSize from "../constants/FontSize";
 
-export default function SignIn({navigation}: any) {
-  const image = require('../assets/images/auth-screen.png')
+export default function SignIn({ navigation }: any) {
+  const image = require("../assets/images/main-auth-bg.png");
   const [loginParams, setLoginParams] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   const toast = useToast();
 
   const setEmail = value => {
-    setLoginParams({...loginParams, email: value});
+    setLoginParams({ ...loginParams, email: value });
   };
   const setPassword = value => {
-    setLoginParams({...loginParams, password: value});
+    setLoginParams({ ...loginParams, password: value });
   };
 
   const onSignIn = async () => {
-    console.log('%c onSignIn', 'color:fuchsia');
+    console.log("%c onSignIn", "color:fuchsia");
     const response = await register(loginParams.email, loginParams.password);
     if (response.success) {
-      console.log('response', response);
+      console.log("response", response);
       await createProfileDetails(response.value.user.uid);
       await loadProfileDetails();
-      toast.show('Pomyślnie zarejestrowano się', {type: 'success'});
+      toast.show("Pomyślnie zarejestrowano się", { type: "success" });
     } else {
-      toast.show(String(response.value), {type: 'danger'});
+      toast.show(String(response.value), { type: "danger" });
     }
   };
 
   return (
-    // <View style={s.basicInfo}>
-    //   <Text>SignIn</Text>
-    //   <Button title={'Login'} onPress={() => navigation.push('Login')} />
-    //
-    //   <TextInput
-    //     onChangeText={setEmail}
-    //     value={loginParams.email}
-    //     placeholder={'Email'}
-    //   />
-    //   <TextInput
-    //     onChangeText={setPassword}
-    //     value={loginParams.password}
-    //     placeholder={'Password'}
-    //   />
-    //   <Button title={'Zarejestruj się'} onPress={() => onSignIn()} />
-    // </View>
     <View style={s.container}>
       <ImageBackground source={image} resizeMode="cover" style={s.image}>
-        <View style={s.basicInfo}>
-          <TextInput
-            onChangeText={setEmail}
-            value={loginParams.email}
-            placeholder={'Email'}
-            style={{marginTop:Spacing.sm}}
-          />
-          <TextInput
-            onChangeText={setPassword}
-            value={loginParams.password}
-            placeholder={'Password'}
-            secureTextEntry={true}
-            style={{marginTop:Spacing.sm}}
-          />
-          <View style={{marginTop:Spacing.md, marginBottom: Spacing.lg}}>
-            <Button title={'Zarejestruj się'} onPress={onSignIn} />
+        <View style={{ marginTop: -150 }}>
+          <View style={s.logo}>
+            <ImageBackground source={require("../assets/images/main-logo.png")} resizeMode="cover" style={s.image} />
           </View>
-          <View style={{marginTop:Spacing.xl, justifyContent:"space-around", alignItems: "center"}}>
-            <View style={{flexDirection: 'row'}}>
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.push('Login');
-                }}
-              >
-                <Text style={{fontSize: FontSize.h5, marginLeft:Spacing.sm, borderBottomColor:'white', borderBottomWidth: 1}}>Wróć do logowania</Text>
-              </TouchableOpacity>
+          <View style={s.basicInfo}>
+            <TextInput
+              onChangeText={setEmail}
+              value={loginParams.email}
+              placeholder={"Email"}
+              style={{ marginTop: Spacing.sm }}
+            />
+            <TextInput
+              onChangeText={setPassword}
+              value={loginParams.password}
+              placeholder={"Password"}
+              secureTextEntry={true}
+              style={{ marginTop: Spacing.sm }}
+            />
+            <View style={{ marginTop: Spacing.md, marginBottom: Spacing.lg }}>
+              <Button title={"Zarejestruj się"} onPress={onSignIn} />
+            </View>
+            <View style={{ marginTop: Spacing.xl, justifyContent: "space-around", alignItems: "center" }}>
+              <View style={{ flexDirection: "row" }}>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("Login");
+                  }}
+                >
+                  <Text style={{
+                    fontSize: FontSize.h5,
+                    marginLeft: Spacing.sm,
+                    borderBottomColor: "white",
+                    borderBottomWidth: 1,
+                  }}>Wróć do logowania</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </View>
@@ -96,21 +87,31 @@ export default function SignIn({navigation}: any) {
 const s = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    flexDirection: 'column',
+    width: "100%",
+    flexDirection: "column",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   basicInfo: {
-    marginVertical: Spacing.xl,
-    width: '100%'
+    marginTop: 80,
+    marginBottom: Spacing.xs,
+    width: "100%",
+    backgroundColor: "rgba(52, 52, 52, 0.5)",
+    paddingHorizontal: Spacing.md,
+    paddingVertical: Spacing.xl,
   },
   image: {
     flex: 1,
     justifyContent: "center",
-    with: '100%'
+    with: "100%",
+  },
+  logo: {
+    height: 128,
+    width: "100%",
+    justifyContent: "flex-start",
+    paddingHorizontal: Spacing.xl,
   },
 });
